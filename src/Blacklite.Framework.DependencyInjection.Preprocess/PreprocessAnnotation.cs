3d6121c53e62c681097ea3543788fecb295d7191
David Driscoll <david.driscoll@gmail.com>
2014-12-06 13:40:21 -0500

    Upgraded to beta2 Made compilers work for both AddAssembly and FromAssembly.

--- src/Blacklite.Framework.DependencyInjection.Preprocess/PreprocessAnnotation.cs
+++ src/Blacklite.Framework.DependencyInjection.Preprocess/PreprocessAnnotation.cs
@@ -31,17 +31,71 @@ namespace Blacklite.Framework.DependencyInjection.compiler.preprocess
         private IEnumerable<Container<InvocationExpressionSyntax, IMethodSymbol>> GetAddAssemblyMethodCall(SyntaxTree syntaxTree, SemanticModel model)
         {
             // Find all classes that have our attribute.
-            var AddAssemblyExpressions = syntaxTree
+            var addAssemblyExpressions = syntaxTree
                        .GetRoot()
                        .DescendantNodes()
                        .OfType<MemberAccessExpressionSyntax>()
                        .Where(declaration => declaration.Name.ToString().Contains("AddAssembly"))
                        .Where(declaration => declaration.Parent is InvocationExpressionSyntax)
                        .Where(declaration => model.GetSymbolInfo(declaration.Parent).Symbol is IMethodSymbol)
-                       .Select(declaration => new Container<InvocationExpressionSyntax, IMethodSymbol>((InvocationExpressionSyntax)declaration.Parent, (IMethodSymbol)model.GetSymbolInfo(declaration.Parent).Symbol));
+                       .Select(declaration => new Container<InvocationExpressionSyntax, IMethodSymbol>((InvocationExpressionSyntax)declaration.Parent, (IMethodSymbol)model.GetSymbolInfo(declaration.Parent).Symbol))
+                       .Where(x => x.Symbol.IsExtensionMethod)
+                       .Where(x => x.Symbol.ReceiverType.ToString() == "Microsoft.Framework.DependencyInjection.IServiceCollection");
 
             // Each return the container for each class
-            foreach (var expression in AddAssemblyExpressions)
+            foreach (var expression in addAssemblyExpressions)
+            {
+                if (expression.Declaration.ArgumentList.Arguments.Count() == 1)
+                {
+                    ClassDeclarationSyntax classSyntax;
+                    SyntaxNode parent = expression.Declaration;
+                    while (!(parent is ClassDeclarationSyntax))
+                    {
+                        parent = parent.Parent;
+                    }
+                    classSyntax = parent as ClassDeclarationSyntax;
+
+                    var replace = false;
+
+                    var typeofExpression = expression.Declaration.ArgumentList.Arguments[0].Expression as TypeOfExpressionSyntax;
+                    if (typeofExpression != null)
+                    {
+                        if (typeofExpression.Type.ToString() == classSyntax.Identifier.ToString())
+                        {
+                            replace = true;
+                        }
+                    }
+
+                    var thisExpression = expression.Declaration.ArgumentList.Arguments[0].Expression as ThisExpressionSyntax;
+                    if (thisExpression != null)
+                    {
+                        replace = true;
+                    }
+
+                    if (replace)
+                    {
+                        yield return expression;
+                    }
+                }
+            }
+        }
+
+        private IEnumerable<Container<InvocationExpressionSyntax, IMethodSymbol>> GetFromAssemblyMethodCall(SyntaxTree syntaxTree, SemanticModel model)
+        {
+            // Find all classes that have our attribute.
+            var fromAssemblyExpressions = syntaxTree
+                       .GetRoot()
+                       .DescendantNodes()
+                       .OfType<MemberAccessExpressionSyntax>()
+                       .Where(declaration => declaration.Name.ToString().Contains("FromAssembly"))
+                       .Where(declaration => declaration.Parent is InvocationExpressionSyntax)
+                       .Where(declaration => model.GetSymbolInfo(declaration.Parent).Symbol is IMethodSymbol)
+                       .Select(declaration => new Container<InvocationExpressionSyntax, IMethodSymbol>((InvocationExpressionSyntax)declaration.Parent, (IMethodSymbol)model.GetSymbolInfo(declaration.Parent).Symbol))
+                       .Where(x => x.Symbol.IsExtensionMethod)
+                       .Where(x => x.Symbol.ReceiverType.ToString() == "Microsoft.Framework.DependencyInjection.ServiceDescriber");
+
+            // Each return the container for each class
+            foreach (var expression in fromAssemblyExpressions)
             {
                 if (expression.Declaration.ArgumentList.Arguments.Count() == 1)
                 {
@@ -148,27 +202,38 @@ namespace Blacklite.Framework.DependencyInjection.compiler.preprocess
             return nameSyntax;
         }
 
-        public IEnumerable<Func<string, IEnumerable<StatementSyntax>>> GetStatements(IBeforeCompileContext context, IEnumerable<Container<ClassDeclarationSyntax, INamedTypeSymbol>> containers)
+        private static string GetDisplayString(INamedTypeSymbol symbol)
+        {
+            string implementationType;
+            if (symbol.IsGenericType)
+                implementationType = symbol.ConstructUnboundGenericType().ToDisplayString();
+            else
+                implementationType = symbol.ToDisplayString();
+            return implementationType;
+        }
+
+        public IEnumerable<Func<string, IEnumerable<TStatement>>> GetStatements<TStatement>(IBeforeCompileContext context, IEnumerable<Container<ClassDeclarationSyntax, INamedTypeSymbol>> containers, Func<string, IEnumerable<NameSyntax>, NameSyntax, Func<string, IEnumerable<TStatement>>> getStatement)
         {
             foreach (var container in containers)
             {
-                yield return GetStatement(context, container.Symbol, container.Declaration);
+                yield return GetStatement(context, container.Symbol, container.Declaration, getStatement);
             }
         }
 
-        public Func<string, IEnumerable<StatementSyntax>> GetStatement(IBeforeCompileContext context, INamedTypeSymbol symbol, ClassDeclarationSyntax declaration)
+        public Func<string, IEnumerable<TStatement>> GetStatement<TStatement>(IBeforeCompileContext context, INamedTypeSymbol symbol, ClassDeclarationSyntax declaration, Func<string, IEnumerable<NameSyntax>, NameSyntax, Func<string, IEnumerable<TStatement>>> getStatement)
         {
             var attributeSymbol = symbol.GetAttributes().Single(x => x.AttributeClass.Name.ToString().Contains("ServiceDescriptorAttribute"));
             var attributeDeclaration = declaration.AttributeLists
                 .SelectMany(z => z.Attributes)
                 .Single(z => z.Name.ToString().Contains("ServiceDescriptor"));
 
-            var implementationType = symbol.ConstructUnboundGenericType().ToDisplayString();
+            string implementationType = GetDisplayString(symbol);
+
             var implementationQualifiedName = BuildQualifiedName(implementationType, symbol.TypeParameters.Count());
 
             string serviceType = null;
             IEnumerable<NameSyntax> serviceQualifiedNames = symbol.AllInterfaces
-                .Select(z => BuildQualifiedName(z.ConstructUnboundGenericType().ToDisplayString(), z.TypeParameters.Count()));
+                .Select(z => BuildQualifiedName(GetDisplayString(z), z.TypeParameters.Count()));
 
             if (declaration.Modifiers.Any(z => z.RawKind == (int)SyntaxKind.PublicKeyword))
             {
@@ -181,7 +246,7 @@ namespace Blacklite.Framework.DependencyInjection.compiler.preprocess
                 var serviceNameTypedSymbol = (INamedTypeSymbol)attributeSymbol.ConstructorArguments[0].Value;
                 if (serviceNameTypedSymbol == null)
                     throw new Exception("Could not infer service symbol");
-                serviceType = serviceNameTypedSymbol.ConstructUnboundGenericType().ToString();
+                serviceType = GetDisplayString(serviceNameTypedSymbol);
                 serviceQualifiedNames = new NameSyntax[] { BuildQualifiedName(serviceType, serviceNameTypedSymbol.TypeParameters.Count()) };
             }
 
@@ -196,9 +261,7 @@ namespace Blacklite.Framework.DependencyInjection.compiler.preprocess
 
             // TODO: Enforce implementation is assignable to service
             // Diagnostic error?
-            var potentialBaseTypes = baseTypes.Concat(
-                symbol.AllInterfaces.Select(z => z.ConstructUnboundGenericType().ToDisplayString())
-            );
+            var potentialBaseTypes = baseTypes.Concat(symbol.AllInterfaces.Select(GetDisplayString));
 
             // This is where some of the power comes out.
             // We now have the ability to throw compile time errors if we believe something is wrong.
@@ -234,7 +297,7 @@ namespace Blacklite.Framework.DependencyInjection.compiler.preprocess
             }
 
             // Build the Statement
-            return GetCollectionExpressionStatement(lifecycle, serviceQualifiedNames, implementationQualifiedName);
+            return getStatement(lifecycle, serviceQualifiedNames, implementationQualifiedName);
         }
 
         public string GetLifecycle(int enumValue)
@@ -253,7 +316,7 @@ namespace Blacklite.Framework.DependencyInjection.compiler.preprocess
             return lifecycle;
         }
 
-        public Func<string, IEnumerable<StatementSyntax>> GetCollectionExpressionStatement(string lifecycle, IEnumerable<NameSyntax> serviceQualifiedNames, NameSyntax implementationQualifiedName)
+        public Func<string, IEnumerable<StatementSyntax>> GetCollectionAddAssemblyExpressionStatement(string lifecycle, IEnumerable<NameSyntax> serviceQualifiedNames, NameSyntax implementationQualifiedName)
         {
             // I hear there is a better way to do this... that will be released sometime.
             return (string identifierName) =>
@@ -278,6 +341,30 @@ namespace Blacklite.Framework.DependencyInjection.compiler.preprocess
             };
         }
 
+        public Func<string, IEnumerable<ExpressionSyntax>> GetCollectionFromAssemblyExpressionStatement(string lifecycle, IEnumerable<NameSyntax> serviceQualifiedNames, NameSyntax implementationQualifiedName)
+        {
+            // I hear there is a better way to do this... that will be released sometime.
+            return (string identifierName) =>
+            {
+                return serviceQualifiedNames.Select(serviceName =>
+                    SyntaxFactory.InvocationExpression(
+                        SyntaxFactory.MemberAccessExpression(
+                            SyntaxKind.SimpleMemberAccessExpression,
+                            SyntaxFactory.IdentifierName(identifierName),
+                            name: SyntaxFactory.IdentifierName(SyntaxFactory.Identifier(lifecycle))
+                        ),
+                        SyntaxFactory.ArgumentList(
+                            SyntaxFactory.SeparatedList(
+                                new[] {
+                                    SyntaxFactory.Argument(SyntaxFactory.TypeOfExpression(serviceName)),
+                                    SyntaxFactory.Argument(SyntaxFactory.TypeOfExpression(implementationQualifiedName))
+                                })
+                        )
+                    )
+                );
+            };
+        }
+
         public void BeforeCompile(IBeforeCompileContext context)
         {
             // Find all the class containers we care about.
@@ -289,39 +376,84 @@ namespace Blacklite.Framework.DependencyInjection.compiler.preprocess
                     Root = tree.GetRoot()
                 });
 
-
-            var addAssemblyMethodCalls = containers
-                .SelectMany(ctx => GetAddAssemblyMethodCall(ctx.SyntaxTree, ctx.Model));
-
-            // Build the registration statements out of the containers.
-            var nodes = GetStatements(context, containers.SelectMany(ctx => GetClassesWithImplementationAttribute(ctx.SyntaxTree, ctx.Model)))
-                .ToArray();
-
             var newCompilation = context.CSharpCompilation;
 
-            foreach (var method in addAssemblyMethodCalls)
-            {
-                var identifierName = ((MemberAccessExpressionSyntax)method.Declaration.Expression).Expression.ToString();
+            // Build the registration statements out of the containers.
+            var addAssemblyNodes = GetStatements(context, containers.SelectMany(ctx => GetClassesWithImplementationAttribute(ctx.SyntaxTree, ctx.Model)), GetCollectionAddAssemblyExpressionStatement).ToArray();
+            var addAssemblyMethodCalls = containers.SelectMany(ctx => GetAddAssemblyMethodCall(ctx.SyntaxTree, ctx.Model));
+            newCompilation = ProcessAddAssemblyMethodCalls(newCompilation, addAssemblyNodes, addAssemblyMethodCalls);
 
-                var methodNodes = nodes.SelectMany(x => x(identifierName));
-
-                var oldSyntaxRoot = method.Declaration.SyntaxTree.GetRoot();
-                //var newSyntaxRoot = oldSyntaxRoot;
-                //foreach (var newNode in methodNodes) {
-                //    newSyntaxRoot = newSyntaxRoot.ReplaceNodes(new[] { method.Declaration.Parent }, (o, r) => newNode);
-                //}
-                var newSyntaxRoot = oldSyntaxRoot
-                    .InsertNodesAfter(method.Declaration.Parent, methodNodes)
-                    .RemoveNode(method.Declaration.Parent, SyntaxRemoveOptions.KeepExteriorTrivia)
-                    .NormalizeWhitespace();
-
-                var oldSyntaxTree = method.Declaration.SyntaxTree;
-                var newSyntaxTree = oldSyntaxTree.WithRootAndOptions(newSyntaxRoot, oldSyntaxTree.Options);
-
-                newCompilation = newCompilation.ReplaceSyntaxTree(oldSyntaxTree, newSyntaxTree);
-            }
+            var fromAssemblyNodes = GetStatements(context, containers.SelectMany(ctx => GetClassesWithImplementationAttribute(ctx.SyntaxTree, ctx.Model)), GetCollectionFromAssemblyExpressionStatement).ToArray();
+            var fromAssemblyMethodCalls = containers.SelectMany(ctx => GetFromAssemblyMethodCall(ctx.SyntaxTree, ctx.Model));
+            newCompilation = ProcessFromAssemblyMethodCalls(newCompilation, fromAssemblyNodes, fromAssemblyMethodCalls);
 
             context.CSharpCompilation = newCompilation;
+        }
+
+        private CSharpCompilation ProcessAddAssemblyMethodCalls(CSharpCompilation compilation, IEnumerable<Func<string, IEnumerable<StatementSyntax>>> nodes, IEnumerable<Container<InvocationExpressionSyntax, IMethodSymbol>> containers)
+        {
+            foreach (var methods in containers.GroupBy(m => m.Declaration.SyntaxTree))
+            {
+                SyntaxNode syntaxRoot = null;
+                foreach (var method in methods)
+                {
+                    if (syntaxRoot == null)
+                        syntaxRoot = method.Declaration.SyntaxTree.GetRoot();
+
+                    var identifierName = ((MemberAccessExpressionSyntax)method.Declaration.Expression).Expression.ToString();
+
+                    var methodNodes = nodes.SelectMany(x => x(identifierName));
+
+                    syntaxRoot = syntaxRoot
+                        .ReplaceNode(method.Declaration.Parent, methodNodes)
+                        .NormalizeWhitespace();
+                }
+
+                var syntaxTree = methods.Key;
+                var newSyntaxTree = syntaxTree.WithRootAndOptions(syntaxRoot, syntaxTree.Options);
+
+                compilation = compilation.ReplaceSyntaxTree(syntaxTree, newSyntaxTree);
+            }
+
+            return compilation;
+        }
+
+        private CSharpCompilation ProcessFromAssemblyMethodCalls(CSharpCompilation compilation, IEnumerable<Func<string, IEnumerable<ExpressionSyntax>>> nodes, IEnumerable<Container<InvocationExpressionSyntax, IMethodSymbol>> containers)
+        {
+            foreach (var methods in containers.GroupBy(m => m.Declaration.SyntaxTree))
+            {
+                SyntaxNode syntaxRoot = null;
+                foreach (var method in methods)
+                {
+                    if (syntaxRoot == null)
+                    {
+                        syntaxRoot = method.Declaration.SyntaxTree.GetRoot();
+                    }
+
+                    var identifierName = ((MemberAccessExpressionSyntax)method.Declaration.Expression).Expression.ToString();
+
+                    var methodNodes = nodes.SelectMany(x => x(identifierName));
+                    var node = SyntaxFactory.ExpressionStatement(
+                        SyntaxFactory.ImplicitArrayCreationExpression(
+                            SyntaxFactory.InitializerExpression(
+                                SyntaxKind.ArrayInitializerExpression,
+                                SyntaxFactory.SeparatedList(methodNodes)
+                            )
+                        )
+                    );
+
+                    syntaxRoot = syntaxRoot
+                       .ReplaceNode(method.Declaration, node.Expression)
+                       .NormalizeWhitespace();
+                }
+
+                var syntaxTree = methods.Key;
+                var newSyntaxTree = syntaxTree.WithRootAndOptions(syntaxRoot, syntaxTree.Options);
+
+                compilation = compilation.ReplaceSyntaxTree(syntaxTree, newSyntaxTree);
+            }
+
+            return compilation;
         }
 
         public void AfterCompile(IAfterCompileContext context)
